66f80119b80f6a59af174feba4ef98a18478a8fc
Valeriy Nefyodov <valeriy.nefyodov@gmail.com>
2018-09-15 13:22:21 +0300

    update time-server to use env variables

--- 01-time-server/index.js
+++ 01-time-server/index.js
@@ -1,21 +1,10 @@
-const args = require('yargs')
-  .usage('$0 [args]')
-  .option('i', {
-    alias: 'interval',
-    default: 500,
-    describe: 'Server date console log interval',
-    type: 'string'
-  })
-  .option('t', {
-    alias: 'timeout',
-    default: 3000,
-    describe: 'response timeout',
-    type: 'string'
-  })
-  .help()
-  .argv;
+require('dotenv').config();
 
 const simpleServer = require('./simple-server');
-const {interval, timeout} = args;
 
-simpleServer.start(interval, timeout)
+const {
+  SERVER_INTERVAL,
+  SERVER_TIMEOUT
+} = process.env;
+
+simpleServer.start(SERVER_INTERVAL, SERVER_TIMEOUT)
